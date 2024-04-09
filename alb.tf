3793cc80160d5ff2a50a3dfc350d2fe427318f4f
ShadowproofGamer <57487079+ShadowproofGamer@users.noreply.github.com>
2024-04-09 07:39:24 +0200

    trying eip with FARGATE

--- alb.tf
+++ alb.tf
@@ -1,11 +1,15 @@
 resource "aws_lb" "ecs_alb_ttt" {
   name               = "ecs-alb-ttt"
-  load_balancer_type = "application"
+  load_balancer_type = "network"
   subnets            = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
   security_groups    = [aws_security_group.allow_web.id]
   tags = {
     Name = "ecs-alb-ttt"
   }
+#  enable_deletion_protection = false
+#  depends_on = [aws_eip.static_ip]
+#  ip_address_type = "ipv4"
+#  load_balancer_ip = aws_eip.static_ip.public_ip
 }
 resource "aws_lb_target_group" "ecs_tg_ttt" {
   name        = "ecs-target-group-ttt"
@@ -35,6 +39,26 @@ resource "aws_lb_listener" "ecs_alb_listener-2" {
     target_group_arn = aws_lb_target_group.ecs_tg_ttt.arn
   }
 }
+#resource "aws_eip" "static_ip" {
+#  domain = "vpc"
+#}
+
+
+
+
+
+
+
+
+
+
 output "load_balancer_ip" {
   value = aws_lb.ecs_alb_ttt.dns_name
+}
+output "nlb_dns_name" {
+  value = aws_lb.ecs_alb_ttt.dns_name
+}
+
+output "nlb_elastic_ip" {
+  value = aws_eip.static_ip.public_ip
 }
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 # 1. Create vpc
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
